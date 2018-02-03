2bc60657578a325428c5070266c6f8ec1916dedf
ysnrkdm <ysnr.kdm@gmail.com>
2018-02-03 15:05:30 +0900

    improved calc

--- calc.py
+++ calc.py
@@ -43,8 +43,21 @@ def trade_summary(row):
     to_info = "%s %s [%s]" % (row['to_amount'], row['to_unit'], row['to_rate_jpy'])
     return "%s %s -> %s at %s" % (row['category'], from_info, to_info, row['time'])
 
+verbose = False
+output_tab_delimited_rows = not verbose
+
+if output_tab_delimited_rows:
+    delimiter = "\t"
+    header = ['time', 'category', 'from_amount', 'from_unit', 'from_rate_jpy',
+              'to_amount', 'to_unit', 'to_rate_jpy', 'PnL total', 'PnL single']
+    print delimiter.join(header)
+
 for row in get_table_for_scheme(currency):
-    print trade_summary(row)
+    if verbose:
+        print trade_summary(row)
+
+    earning_by_this_sell = 0
+
     if row['to_unit'] == currency:
         if row['category'] == 'buys':
             avg_price = (avg_price * total_bch + row['to_rate_jpy'] * row['to_amount']) / (total_bch + row['to_amount'])
@@ -54,6 +67,21 @@ for row in get_table_for_scheme(currency):
         total_bch = max(0, total_bch - row['from_amount'])
         total_bch_raw = total_bch_raw - row['from_amount']
         if row['category'] == 'sells':
-            earning_by_this_sell = row['from_amount'] * (row['from_rate_jpy'] - avg_price)
+            if row['to_unit'] == 'JPY':
+                earning_by_this_sell = row['from_amount'] * (row['from_rate_jpy'] - avg_price)
+            else:
+                earning_by_this_sell = row['to_rate_jpy'] - avg_price * row['from_amount']
+            if verbose:
+                print " -- PnL is %s" % (str(earning_by_this_sell))
             earning += earning_by_this_sell
-    print "  -- Avg %s price is %s, total %s %s. Total earning is %s" % (currency, str(avg_price), currency, str(total_bch_raw), str(earning))
+    if verbose:
+        print "  -- Avg %s price is %s, total %s %s. Total earning is %s" % (currency, str(avg_price), currency, str(total_bch_raw), str(earning))
+
+    if output_tab_delimited_rows:
+        delimiter = "\t"
+        row = [row['time'], row['category'],
+               row['from_amount'], row['from_unit'], row['from_rate_jpy'],
+               row['to_amount'], row['to_unit'], row['to_rate_jpy'],
+               earning_by_this_sell, earning]
+        row_string = map(lambda x: str(x), row)
+        print delimiter.join(row_string)
